79a3bdb54ae802a721818050bfa7f2d9093a90d6
Mike Rivera <michaelrivera@michaels-mbp-2.attlocal.net>
2019-02-28 17:04:09 -0800

    Created initial files, Created database, and psuedo-coded js

---
+++ bamazonCustomer.js
@@ -0,0 +1,27 @@
+// Minimum requirements
+
+// Require NPM Packages
+
+
+// 1. Display all 10 products for sale (ids, names, and prices)
+
+
+// 2. Promt user 2 messages
+    // 1. What is the id of the item you would like to purchase?
+    // 2. How many units of the item would you like to purchase?
+
+
+// 3. Attempt to place order and check if the store has enough stock to process request
+    // If yes, process order (update database, show customer total cost aka receipt)
+    // If no, cancel transaction and display "Insufficient stock"
+
+
+// ====================================================================================
+
+// Manager View (Next Level)
+
+
+
+// ====================================================================================
+
+// Supervisor View (Final Level)
